177e48aa3d0920b664d11d0216177042bd40e33b
abargstaedt <andreas@bargstaedt.net>
2020-06-12 21:26:06 +0200

    Refactoring

--- speedtest.py
+++ speedtest.py
@@ -5,13 +5,10 @@ import subprocess
 import time
 
 
-def get_speedtest():
-    response = subprocess.Popen('speedtest-cli --simple',
-                                shell=True, stdout=subprocess.PIPE).stdout.read().decode('utf-8')
-
-    ping = re.findall(r'Ping:\s(.*?)\s', response, re.MULTILINE)
-    download = re.findall(r'Download:\s(.*?)\s', response, re.MULTILINE)
-    upload = re.findall(r'Upload:\s(.*?)\s', response, re.MULTILINE)
+def parse_response(res):
+    ping = re.findall(r'Ping:\s(.*?)\s', res, re.MULTILINE)
+    download = re.findall(r'Download:\s(.*?)\s', res, re.MULTILINE)
+    upload = re.findall(r'Upload:\s(.*?)\s', res, re.MULTILINE)
 
     return [
         ping[0].replace(',', '.'),
@@ -20,12 +17,21 @@ def get_speedtest():
     ]
 
 
-[ping, download, upload] = get_speedtest()
+def write_response(res):
+    [ping, download, upload] = parse_response(res)
 
-with open('speedtest.csv', 'a+') as f:
+    with open('speedtest.csv', 'a+') as f:
 
-    if os.stat('speedtest.csv').st_size == 0:
-        f.write('Date,Time,Ping (ms),Download (Mbit/s),Upload (Mbit/s)\r\n')
+        if os.stat('speedtest.csv').st_size == 0:
+            f.write('Date,Time,Ping (ms),Download (Mbit/s),Upload (Mbit/s)\r\n')
 
-    f.write('{},{},{},{},{}\r\n'.format(time.strftime('%m/%d/%y'),
-                                        time.strftime('%H:%M'), ping, download, upload))
+        f.write('{},{},{},{},{}\r\n'.format(time.strftime('%m/%d/%y'),
+                                            time.strftime('%H:%M'), ping, download, upload))
+
+
+res = subprocess.Popen('speedtest-cli --simple',
+                       shell=True, stdout=subprocess.PIPE).stdout.read().decode('utf-8')
+
+print(res)
+
+write_response(res)
